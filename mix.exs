27be003e9af55eed29ce9e2d46de83b7281697fa
Ahmet <ahmetabdi@gmail.com>
2016-10-02 02:07:30 -0700

    Add escript config to mix.exs file

--- mix.exs
+++ mix.exs
@@ -7,25 +7,18 @@ defmodule FizzbuzzCli.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     escript: escript]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  def escript do
+    [main_module: FizzbuzzCli]
+  end
+
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
